3dba30097d7d36ec9122879d0627afaa5611537d
Okera Johnson <okerajohnson@gmail.com>
2024-09-13 09:05:27 -0400

    rnative app

---
+++ app.js
@@ -0,0 +1,46 @@
+import { StatusBar } from "expo-status-bar";
+import React from 'react';
+import { StyleSheet, Text, View, Button } from 'react-native';
+
+export default class App extends React.Component {
+
+    state = {
+        value: 0,
+        total_taps: 0
+    }
+
+    incrementValue = () => {
+        this.setState({
+            value: this.state.value + 1,
+            total_taps: this.state.total_taps + 1
+        })
+        console.log("Value: " + (this.state.value + 1))
+    }
+
+    decrementValue = () => {
+        this.setState({
+            value: this.state.value - 1,
+            total_taps: this.state.total_taps - 1
+        })
+        console.log("Value: " + (this.state.value - 1))
+    }
+
+    render () {
+    return (
+        <View style={styles.container}>
+        <Text> Open up and work neow! </Text>
+        <StatusBar style="auto" />
+        </View>
+    );
+    }
+}
+
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: '#fff',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+});
